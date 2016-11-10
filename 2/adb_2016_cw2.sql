90d2bca4148172cbe48e359e22aa71f9fa81121c
Ravi Woods <rbw14@vm-shell1.doc.ic.ac.uk>
2016-11-10 01:35:12 +0000

    CW2-Finished probs

--- 2/adb_2016_cw2.sql
+++ 2/adb_2016_cw2.sql
@@ -4,7 +4,7 @@ SELECT state.name AS state_name, place.name AS name
 FROM state JOIN place ON state.code = place.state_code
 WHERE place.name LIKE '%City'
 AND type <> 'city'
-ORDER BY state_name, name ASC;
+ORDER BY state.name, name ASC;
 
 
 -- Q2 returns (state_name,no_big_city,big_city_population)
@@ -15,9 +15,9 @@ SELECT 	state.name AS state_name,
 FROM state JOIN place ON state.code = place.state_code
 WHERE type = 'city'
 AND population > 100000
-GROUP BY state_name
+GROUP BY state.name
 HAVING SUM(population) >= 1000000 OR COUNT(place.name) >= 5
-ORDER BY state_name;
+ORDER BY state.name;
 
 
 
@@ -46,10 +46,10 @@ ORDER BY 	all_types.type;
 -- Q4 returns (name,population,pc_population,land_area,pc_land_area)
 
 SELECT 	state.name AS name,
-		SUM(mcd.population) AS population,
-		ROUND((100.0*SUM(mcd.population))/total_population.population,2) AS pc_population,
-		SUM(mcd.land_area) AS land_area,
-		ROUND((100.0*SUM(mcd.land_area))/total_land_area.land_area,2) AS pc_land_area
+		CAST(SUM(mcd.population) AS BIGINT) AS population,
+		ROUND(100.0*SUM(mcd.population)/total_population.population,2) AS pc_population,
+		CAST(SUM(mcd.land_area) AS BIGINT) AS land_area,
+		ROUND(100.0*SUM(mcd.land_area)/total_land_area.land_area,2) AS pc_land_area
 FROM		state LEFT JOIN mcd ON state.code = mcd.state_code,
 		(SELECT SUM(population) AS population FROM mcd) total_population,
 		(SELECT SUM(land_area) AS land_area FROM mcd) total_land_area
