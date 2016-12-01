0a942e8f77094c71f0c17f41707b16916bac0d5d
Ravi Woods <rbw14@point44.doc.ic.ac.uk>
2016-12-01 00:21:00 +0000

    CW2-Q4-FINISHED

--- 3/q1_dir/test.sh
+++ 3/q1_dir/test.sh
@@ -1,3 +1,3 @@
 rm -rf q1
 pig -x local q1.pig
-nano q1/part-r-00000
+sdiff test_code q1/part-r-00000
--- 3/q2_dir/test.sh
+++ 3/q2_dir/test.sh
@@ -1,3 +1,3 @@
 rm -rf q2
 pig -x local q2.pig
-nano q2/part-r-00000
+sdiff test_code q2/part-r-00000
